16958cfdba222dbb1016cc681c4d5c750c99e550
Toni Češarek <programmertoni@gmail.com>
2016-01-16 12:41:57 +0100

    shortens #to_roman_numerals method

--- converter.rb
+++ converter.rb
@@ -1,18 +1,18 @@
 class Converter
   CONVERSIONS = {
-    1    => 'I',
-    4    => 'IV',
-    5    => 'V',
-    9    => 'IX',
-    10   => 'X',
-    40   => 'XL',
-    50   => 'L',
-    90   => 'XC',
-    100  => 'C',
-    400  => 'CD',
-    500  => 'D',
+    1000 => 'M',
     900  => 'CM',
-    1000 => 'M'
+    500  => 'D',
+    400  => 'CD',
+    100  => 'C',
+    90   => 'XC',
+    50   => 'L',
+    40   => 'XL',
+    10   => 'X',
+    9    => 'IX',
+    5    => 'V',
+    4    => 'IV',
+    1    => 'I'
   }
 
   attr_reader :input
@@ -26,16 +26,15 @@ class Converter
       return 'The input must be between 1 and 3999' 
     end
 
-    result = input.reverse.chars.map.with_index do |number, i|
+    arabic_number = input
+    result        = ''
 
-      case i
-      when 0 then roman_ones(number)
-      when 1 then roman_tens(number)
-      when 2 then roman_hundreds(number)
-      when 3 then roman_thousands(number)
+    CONVERSIONS.keys.each do |number|
+      while arabic_number >= number
+        arabic_number -= number
+        result << CONVERSIONS[number]
       end
-    end.reverse.join
-
+    end
     result
   end
 
@@ -45,7 +44,7 @@ class Converter
     roman_numerals = input
     result          = 0
 
-    CONVERSIONS.values.reverse.each do |roman_symbol|
+    CONVERSIONS.values.each do |roman_symbol|
       while roman_numerals.start_with? roman_symbol
         roman_numerals = roman_numerals
                             .slice(roman_symbol.length, roman_numerals.length)
@@ -53,57 +52,5 @@ class Converter
       end
     end
     result
-  end
-
-  private
-
-  def roman_ones(number)
-    case number
-    when '1' then 'I'
-    when '2' then 'II'
-    when '3' then 'III'
-    when '4' then 'IV'
-    when '5' then 'V'
-    when '6' then 'VI'
-    when '7' then 'VII'
-    when '8' then 'VIII'
-    when '9' then 'IX'
-    end
-  end
-
-  def roman_tens(number)
-    case number
-    when '1' then 'X'
-    when '2' then 'XX'
-    when '3' then 'XXX'
-    when '4' then 'XL'
-    when '5' then 'L'
-    when '6' then 'LX'
-    when '7' then 'LXX'
-    when '8' then 'LXXX'
-    when '9' then 'XC'
-    end
-  end
-
-  def roman_hundreds(number)
-    case number
-    when '1' then 'C'
-    when '2' then 'CC'
-    when '3' then 'CCC'
-    when '4' then 'CD'
-    when '5' then 'D'
-    when '6' then 'DC'
-    when '7' then 'DCC'
-    when '8' then 'DCCC'
-    when '9' then 'CM'
-    end
-  end
-
-  def roman_thousands(number)
-    case number
-    when '1' then 'M'
-    when '2' then 'MM'
-    when '3' then 'MMM'
-    end
   end
 end
--- converter_test.rb
+++ converter_test.rb
@@ -26,7 +26,7 @@ class ConverterTest < Minitest::Test
 
   {1 => 'I', 3 => 'III', 5 => 'V', 10 => 'X', 30 => 'XXX', 50 => 'L', 100 => 'C', 500 => 'D', 1000 => 'M'}.each do |key, value|
     define_method(:"test_convert_#{key}_to_#{value}") {
-      assert_equal "#{value}", Converter.new("#{key}").to_roman_numerals
+      assert_equal "#{value}", Converter.new(key).to_roman_numerals
     }
 
     define_method(:"test_convert_#{value}_to_#{key}") {
@@ -36,7 +36,7 @@ class ConverterTest < Minitest::Test
 
   {4 => 'IV', 9 => 'IX', 19 => 'XIX', 40 => 'XL', 90 => 'XC', 400 => 'CD', 900 => 'CM'}.each do |key, value|
     define_method(:"test_convert_#{key}_to_#{value}") {
-      assert_equal "#{value}", Converter.new("#{key}").to_roman_numerals
+      assert_equal "#{value}", Converter.new(key).to_roman_numerals
     }
     define_method(:"test_convert_#{value}_to_#{key}") {
       assert_equal key, Converter.new("#{value}").to_arabic_number
@@ -69,31 +69,30 @@ class ConverterTest < Minitest::Test
   end
 
   def test_59_to_LIX
-    assert_equal 'LIX', Converter.new('59').to_roman_numerals
+    assert_equal 'LIX', Converter.new(59).to_roman_numerals
   end
 
   def test_1234_to_MCCXXXIV
-    assert_equal 'MCCXXXIV', Converter.new('1234').to_roman_numerals
+    assert_equal 'MCCXXXIV', Converter.new(1234).to_roman_numerals
   end
 
   def test_1597_to_MDXCVII
-    assert_equal 'MDXCVII', Converter.new('1597').to_roman_numerals
+    assert_equal 'MDXCVII', Converter.new(1597).to_roman_numerals
   end
 
   def test_max_input_3999
-    assert_equal 'The input must be between 1 and 3999', Converter.new('4000').to_roman_numerals
+    assert_equal 'The input must be between 1 and 3999', Converter.new(4000).to_roman_numerals
   end
 
   def test_0_as_input
-    assert_equal 'The input must be between 1 and 3999', Converter.new('0').to_roman_numerals
+    assert_equal 'The input must be between 1 and 3999', Converter.new(0).to_roman_numerals
   end
 
   def test_negative_numbers
-    assert_equal 'The input must be between 1 and 3999', Converter.new('-10').to_roman_numerals
+    assert_equal 'The input must be between 1 and 3999', Converter.new(-10).to_roman_numerals
   end
 
   def test_max_lines
-    skip
     lines = `wc -l "converter.rb"`.strip.split(' ')[0].to_i
     assert lines < 50, "Solution has too many lines #{lines}, expected 50 or less."
   end
